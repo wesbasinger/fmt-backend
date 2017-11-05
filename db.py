dae7c9b61054d179b7104b781545d386de4d040d
Wes Basinger <wbasinger@villagetechschools.org>
2017-11-04 21:20:15 -0500

    Now getting active cast memebers

--- db.py
+++ db.py
@@ -8,8 +8,24 @@ db = client['fmt']
 
 actives = db['actives']
 
+cast = db['cast']
+
 def insert(data):
 
 	actives.insert_one(data)
 
 	return {"success" : True}                                                                                                                                     
+
+def get_active_cast(session):
+
+	cursor = cast.find(
+		{"activeSessions" : {"$in" : [session]}}
+	)
+
+	results = []
+
+	for doc in cursor:
+
+		results.append({"firstName":doc['firstName'], "lastName":doc['lastName']})
+
+	return results
--- handler.py
+++ handler.py
@@ -1,17 +1,34 @@
 import json
 
-from db import insert
+import db
 
+SESSION = "SP18"
 
 def add(event, context):
 
 	data = json.loads(event['body'])
 
-	result = insert(data)
+	result = db.insert(data)
 
 	response = {
 		"body" : json.dumps(result),
 		"statusCode" : 200
+	}
+
+	return response
+
+def get_active_cast(event, context):
+
+	results = db.get_active_cast(SESSION)
+
+	print(results)
+
+	response = {
+		"body" : json.dumps(results),
+		"statusCode" : 200,
+		"headers" : {
+           "Access-Control-Allow-Origin" : "*"
+        },
 	}
 
 	return response
