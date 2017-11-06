cb5d69e4216eb61c5f815ec20c7a65c59c10835c
Wes Basinger <wbasinger@villagetechschools.org>
2017-11-05 19:09:36 -0600

    Checks geolocation on object and inserts it if its good.

--- handler.py
+++ handler.py
@@ -1,21 +1,9 @@
 import json
 
 import db
+import utils
 
 SESSION = "SP18"
-
-def add(event, context):
-
-	data = json.loads(event['body'])
-
-	result = db.insert(data)
-
-	response = {
-		"body" : json.dumps(result),
-		"statusCode" : 200
-	}
-
-	return response
 
 def get_active_cast(event, context):
 
@@ -32,3 +20,61 @@ def get_active_cast(event, context):
 	}
 
 	return response
+
+def sign_in(event, context):
+
+	data = json.loads(event['body'])
+
+	# expects to get a json object as such...
+	# {
+	# 	"name" : string,
+	# 	"castMember" : string,
+	# 	"session" : string,
+	# 	"comments": string,
+	# 	"geolocation" : object
+	# }
+
+	# first run geolocation check on it
+	try:
+
+		geo_check_result = utils.check_geolocation(data['geolocation'])
+
+	except KeyError:
+
+		response = {
+			"body" : json.dumps({"error": True, "message" : "Malformed data."}),
+			"statusCode" : 200,
+			"headers" : {
+	           "Access-Control-Allow-Origin" : "*"
+	        },
+		}
+
+		return response
+
+
+
+	if geo_check_result['error']:
+
+		response = {
+			"body" : json.dumps(geo_check_result),
+			"statusCode" : 200,
+			"headers" : {
+	           "Access-Control-Allow-Origin" : "*"
+	        },
+		}
+
+		return response
+
+	else:
+
+		success_result = db.insert(data)
+
+		response = {
+			"body" : json.dumps(success_result),
+			"statusCode" : 200,
+			"headers" : {
+	           "Access-Control-Allow-Origin" : "*"
+	        },
+		}
+
+		return response
