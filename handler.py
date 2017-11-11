e3a5fef64358067aac55216312d72d2f96e4366c
Wes Basinger <wbasinger@villagetechschools.org>
2017-11-11 09:29:28 -0600

    Calculating elapsed seconds.

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import json
+from time import time
 
 import db
 import utils
@@ -99,7 +100,26 @@ def process_sign_out(event, context):
 
 	sign_in = db.get_single_sign_in(data['activeId'])
 
-	print(sign_in)
+	# sample sign_in shown below
+
+	# {
+	# 	'_id': ObjectId('5a03b6c3b915df000104d56e'),
+	# 	'name': 'Wesl', 'castMemberName':
+	# 	'Amy Basinger', 'castMemberId':
+	# 	'undefined', 'session': 'SP18 - Beauty and the Beast',
+	# 	'comments': 'sdf',
+	# 	'geolocation': {
+	# 		'latitude': 32.6034122,
+	# 		'longitude': -96.86327759999999,
+	# 		'timestamp': 1510192829544
+	# 	}
+	# }
+
+	now = time()
+
+	elapsed_seconds = now - float(sign_in['geolocation']['timestamp'])/1000
+
+	print("Elapsed seconds: ", elapsed_seconds)
 
 	response = {
 		"body" : json.dumps({"success" : True}),
